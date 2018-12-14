a6c11fa328c5bcc27f3681c15745ba2a34eb24be
Joe Mireles <joe.mireles@burnsoft.net>
2018-12-14 15:22:41 -0500

    #1 Added the ability to get the database location from the registry

--- MyGunDBCSVToMyGunCollection/frmMain.cs
+++ MyGunDBCSVToMyGunCollection/frmMain.cs
@@ -7,11 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BurnSoft.Universal;
+using BurnSoft.mgc.convert.MyGunCollection;
+using System.Diagnostics;
 
 namespace MyGunDBCSVToMyGunCollection
 {
     public partial class frmMain : Form
     {
+        private string DatabaseLocation;
+
+        private void Init()
+        {
+            BSRegistry obj = new BSRegistry();
+
+            DatabaseLocation = obj.GetRegSubKeyValue(BurnSoft.mgc.convert.MyGunCollection.Registry.DefaultRegPath, "DataBase", "");
+            Debug.Print(DatabaseLocation);
+
+
+        }
+
         public frmMain()
         {
             InitializeComponent();
@@ -19,7 +34,7 @@ namespace MyGunDBCSVToMyGunCollection
 
         private void frmMain_Load(object sender, EventArgs e)
         {
-
+            Init();
         }
     }
 }
